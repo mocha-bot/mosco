1a60dc2058bf6c4414d7d0f5dff787880c8e5c5c
aldiwildan77 <aldiwild77@gmail.com>
2024-11-15 18:45:51 +0700

    chore: add linter and pre commit hooks

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,9 @@
-import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
+import "@nomiclabs/hardhat-solhint";
+import type { HardhatUserConfig } from "hardhat/config";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.27",
+	solidity: "0.8.27",
 };
 
 export default config;
