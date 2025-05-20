15cdd12b53fc1919951e65f99876fc74e098b05a
aldiwildan77 <aldiwild77@gmail.com>
2025-05-21 04:48:56 +0700

    chore: update deps and fix linter

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,20 +1,19 @@
-import '@nomicfoundation/hardhat-toolbox';
-import { HardhatUserConfig } from 'hardhat/config';
-
-require('dotenv').config();
+import "@nomicfoundation/hardhat-toolbox";
+import "dotenv/config";
+import type { HardhatUserConfig } from "hardhat/config";
 
 const config: HardhatUserConfig = {
-  solidity: '0.8.27',
+  solidity: "0.8.27",
   networks: {
     hardhat: {},
     sepolia: {
-      url: process.env.SEPOLIA_NETWORK_URL || '',
-      accounts: [process.env.PRIVATE_KEY || ''],
+      url: process.env.SEPOLIA_NETWORK_URL || "",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
     },
   },
   etherscan: {
     apiKey: {
-      sepolia: process.env.ETHERSCAN_API_KEY || '',
+      sepolia: process.env.ETHERSCAN_API_KEY || "",
     },
   },
   sourcify: {
